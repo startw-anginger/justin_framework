e8f15987065380ad1f825bc78f26271f88d19930
SuperSonic <supersonic@livemail.tw>
2022-01-20 17:11:59 +0800

    Update

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -9,10 +9,8 @@ $_CONFIG = array();
 $_CONFIG["CORS"] = true;
 $_CONFIG["CORS_DOMAIN"] = "http://localhost:8080";
 
-$_CONFIG["DB_DSN"] = "mysql:host=127.0.0.1;dbname=example";
-$_CONFIG["DB_USERNAME"] = "example";
-$_CONFIG["DB_PASSWORD"] = "example";
-
-$_CONFIG["JWT_SECRET"] = "example";
+$_CONFIG["DB_DSN"] = "mysql:host=127.0.0.1;dbname=justin";
+$_CONFIG["DB_USERNAME"] = "justin_example";
+$_CONFIG["DB_PASSWORD"] = "default_password";
 
 return $_CONFIG;
